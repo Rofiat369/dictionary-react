a02405bb2c70b5686c2842343c5dec8039a23657
Rofiat369 <63071451+Rofiat369@users.noreply.github.com>
2021-05-29 18:44:30 -0400

    Added Axios and integrated API

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
     let [keyword, setKeyword] = useState("")
 
+    function handleResponse(response) {
+        console.log(response.data[0])
+    }
+
     function Search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function KeywordChange(event) {
